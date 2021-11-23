04dd48c409c04c8eaf4f47b36938acfe7ca79c4f
Anton Papa <48538391+hypercurious@users.noreply.github.com>
2021-11-23 09:46:16 +0200

    Delete ask5/work directory

--- ask5/work/test_sync_counter_8bit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity test_sync_counter_8bit is
-end test_sync_counter_8bit;
--- ask5/work/testcounter_8bit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity testcounter_8bit is
-end testcounter_8bit;
